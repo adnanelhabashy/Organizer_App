dd1088ebac519c804b481555918f1ceaa8b6b2e3
adnanelhabashy <adnan.elhabashy@live.com>
2023-06-21 16:06:37 +0300

    Add project files.

---
+++ Organizer_App/Client/Controls/ValidationInputBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Organizer_App.Client.Controls
+{
+    public class ValidationInputBase : ComponentBase
+    {
+        [Parameter]
+        public string placeHolder { get; set; }
+        [Parameter]
+        public string Value { get; set; }
+        [Parameter]
+        public EventCallback<string> ValueChangedCallBack { get; set; }
+
+        public async void HandleInputChange(ChangeEventArgs args)
+        {
+           await ValueChangedCallBack.InvokeAsync(args.Value.ToString());
+        }
+
+    }
+}
